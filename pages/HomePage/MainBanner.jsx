317fc0ce6350444763ee0901fb9ac7cdaa47a17c
Ömer Faruk Ertürk <erturkomer256@gmail.com>
2024-05-07 01:08:15 +0300

    banner image animation added and some files updated

--- pages/HomePage/MainBanner.jsx
+++ pages/HomePage/MainBanner.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ShopButton from "../../components/shopNowButton.jsx";
-import IphoneImage from "./image/bannerIphone.svg"
+import IphoneImage from "./image/bannerIphone.svg";
 import PlayStation from "./image/bannerPlayStation.svg";
 import Airpods from "./image/bannerAirpods.svg";
 import AppleVision from "./image/bannerAppleVision.svg";
-import Mackbook from "./image/bannerMackbook.svg"
+import Mackbook from "./image/bannerMackbook.svg";
 
 const MainBanner = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        // Sayfa scroll edildiğinde bu fonksiyon çalışacak
+        const handleScroll = () => {
+            // Eğer scroll pozisyonu 100 pikselden büyükse resimlerin görünmesini sağlayacak sınıfı ekleyelim
+            if (window.scrollY > 100) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        // Sayfa scroll event listenerını ekleyelim
+        window.addEventListener("scroll", handleScroll);
+
+        // Tbileşen unmount edildiğinde event listeneri kaldıralım
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <div className="main-banner">
@@ -17,15 +39,15 @@ const MainBanner = () => {
                     </div>
                     <div className="main-banner-text">
                         <p>Created to change everything for the better. For everyone</p>
-                        <ShopButton  border="white" color="white" />
+                        <ShopButton border="white" color="white" />
                     </div>
                 </div>
-                <img src={IphoneImage} alt="image bulunamadi." />
+                <img src={IphoneImage} alt="image bulunamadi."/>
             </div>
             <div className="smaller-banners">
                 <div className="left-banners">
                     <div className="wide-square">
-                        <img src={PlayStation} alt="image bulunamadi." />
+                        <img src={PlayStation} alt="image bulunamadi."/>
                         <div className="banner-wide-square-title-text">
                             <h1>Playstation 5</h1>
                             <p>Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.</p>
@@ -33,7 +55,7 @@ const MainBanner = () => {
                     </div>
                     <div className="squares-banner">
                         <div className="square-banner left-bottom-bn-1">
-                            <img src={Airpods} alt="image bulunamadi." />
+                            <img src={Airpods} alt="image bulunamadi."/>
                             <div className="banner-square-title-text">
                                 <h1>Apple AirPods <span>Max</span></h1>
                                 <p>Computational audio. Listen, it's powerful</p>
@@ -56,11 +78,11 @@ const MainBanner = () => {
                         </div>
                         <ShopButton border="black" color="black" gap="8px" />
                     </div>
-                    <img src={Mackbook} alt="imag bulunamadi." />
+                    <img src={Mackbook} alt="imag bulunamadi." className={`${scrolled ? "img-animation" : ""}`}/>
                 </div>
             </div>
         </>
-    )
+    );
 };
 
 export default MainBanner;
