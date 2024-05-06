8869f6e061b718bc094718f617a57850e186a91d
Ömer Faruk Ertürk <erturkomer256@gmail.com>
2024-05-06 20:13:30 +0300

    Added second nested banners to the homepage

--- components/shopNowButton.jsx
+++ components/shopNowButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const shopNowButton = () => {
+const shopNowButton = (props) => {
   return (
     <>
-    <div className="shop-now-button">
+      <div className="shop-now-button" style={{ border: `1px solid ${props.border}`, color: props.color}}>
         Shop Now
-    </div>
+      </div>
     </>
   )
 };
--- pages/HomePage/MainBanner.jsx
+++ pages/HomePage/MainBanner.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ShopButton from "../../components/shopNowButton.jsx";
 import IphoneImage from "./image/bannerIphone.svg"
+import PlayStation from "./image/bannerPlayStation.svg";
+import Airpods from "./image/bannerAirpods.svg";
+import AppleVision from "./image/bannerAppleVision.svg";
+import Mackbook from "./image/bannerMackbook.svg"
 
 const MainBanner = () => {
     return (
@@ -13,10 +17,47 @@ const MainBanner = () => {
                     </div>
                     <div className="main-banner-text">
                         <p>Created to change everything for the better. For everyone</p>
-                        <ShopButton />
+                        <ShopButton  border="white" color="white" />
                     </div>
                 </div>
                 <img src={IphoneImage} alt="image bulunamadi." />
+            </div>
+            <div className="smaller-banners">
+                <div className="left-banners">
+                    <div className="wide-square">
+                        <img src={PlayStation} alt="image bulunamadi." />
+                        <div className="banner-wide-square-title-text">
+                            <h1>Playstation 5</h1>
+                            <p>Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.</p>
+                        </div>
+                    </div>
+                    <div className="squares-banner">
+                        <div className="square-banner left-bottom-bn-1">
+                            <img src={Airpods} alt="image bulunamadi." />
+                            <div className="banner-square-title-text">
+                                <h1>Apple AirPods <span>Max</span></h1>
+                                <p>Computational audio. Listen, it's powerful</p>
+                            </div>
+                        </div>
+                        <div className="square-banner left-bottom-bn-2">
+                            <img src={AppleVision} alt="image bulunamadi." />
+                            <div className="banner-square-title-text">
+                                <h1>Apple <br /> Vision <span>Pro</span></h1>
+                                <p>An immersive way to experience entertainment</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="big-banner">
+                    <div className="big-banner-content">
+                        <div className="big-banner-title-text">
+                            <h1>Macbook <span>Air</span></h1>
+                            <p>The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.</p>
+                        </div>
+                        <ShopButton border="black" color="black" gap="8px" />
+                    </div>
+                    <img src={Mackbook} alt="imag bulunamadi." />
+                </div>
             </div>
         </>
     )
