21a3265bde3efa76a6a8afc07c32caf9cd73ac61
Ömer Faruk Ertürk <erturkomer256@gmail.com>
2024-05-12 17:26:42 +0300

    the smartphones page where we list products is over for now

--- pages/404NOTFOUND/NotFound.jsx
+++ pages/404NOTFOUND/NotFound.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return (
-    <>
-    <div className="404-container" style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",width:"100%",height:"60vh", background:"#232323"}}>
-        <h1 style={{fontSize:"128px", color:"white"}}>404</h1>
-        <h3 style={{textAlign:"center",fontSize:"32px",letterSpacing:"1px",height:"85px",color:"white"}}>Sorry, Page not<br />found</h3>
-        <p style={{fontSize:"24px",height:"48px",color:"white"}}>the page you requested was not found</p>
-        <Link to="/">
-            <button style={{background:"gray",color:"white",border:"0",outline:"none",padding:"16px 32px",cursor:"pointer",borderRadius:"16px"}}>GO BACK HOME</button>
-        </Link>
-    </div>
-    </>
-  )
+    
+    return (
+        <>
+            <div className="404-container" style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", width: "100%", height: "88vh", background: "#232323" }}>
+                <h1 style={{ fontSize: "128px", color: "white" }}>404</h1>
+                <h3 style={{ textAlign: "center", fontSize: "32px", letterSpacing: "1px", height: "85px", color: "white" }}>Sorry, Page not<br />found</h3>
+                <p style={{ fontSize: "24px", height: "48px", color: "white" }}>the page you requested was not found</p>
+                <div className="button-404" style={{display:"flex"}}>
+                    <Link to="/">
+                        <button style={{ margin: "5px", background: "gray", color: "white", border: "0", outline: "none", padding: "16px 32px", cursor: "pointer", borderRadius: "16px" }}>GO BACK HOME</button>
+                    </Link>
+                    <a target="blank" href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvlHzsDsvPWlNfWqxJMNKZRwPDBSLhqCQKNthBzhHTsMSdGCrFTPFZSclRDkBnqjgdFzNB"><button style={{ margin: "5px", background: "white", color: "black", border: "0", outline: "none", padding: "16px 32px", cursor: "pointer", borderRadius: "16px" }}>BİZİMLE İLETİŞİME GEÇİN</button></a>
+                </div>
+            </div>
+        </>
+    )
 };
 
 export default NotFound;
--- pages/ProductPage/ProductList.jsx
+++ pages/ProductPage/ProductList.jsx
@@ -1,18 +1,106 @@
-import React from "react";
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Breadcrumbs from "../../components/allPageComponents/breadCrumbs";
+import Accordion from "./Accordion";
+import Product from '../../components/allPageComponents/products/product';
+
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(import.meta.env.VITE_API_URL + "products");
+      const allProducts = response.data;
+      setProducts(allProducts);
+    } catch (error) {
+      console.error('Ürünleri çekerken bir hata oluştu:', error);
+    }
+  }
+
   const breadcrumbsHierarchy = [
     { name: "Home", link: "/" },
     { name: "Catalog", link: "/catalog" },
     { name: "Smartphones", link: "/catalog/smartphones" }
   ];
+
+  // Sayfadaki son ürünün indeksi
+  const indexOfLastProduct = currentPage * 9;
+  // Sayfadaki ilk ürünün indeksi
+  const indexOfFirstProduct = indexOfLastProduct - 9;
+  // Şu anki sayfadaki ürünler
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Toplam sayfa sayısı
+  const totalPages = Math.ceil(products.length / 9);
+
+  // Sayfa değiştirme işlevi
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <>
-
       <Breadcrumbs categories={breadcrumbsHierarchy} />
+      <div className="product-list-inclusionary">
+        <div className="product-list-container">
+          <div className="product-list-filters-container">
+            <div className="accordion-container">
+              <Accordion name="Brand" />
+              <Accordion name="Battery capacity" />
+              <Accordion name="Screen type" />
+              <Accordion name="Screen diagonal" />
+              <Accordion name="Protection class" />
+              <Accordion name="Built-in memory" />
+            </div>
+          </div>
+          <div className="product-list-products-container">
+            <div className="product-list-top-part">
+              <div className="left-side-products">
+                <h3>Selected Products: <span>{products.length}</span></h3>
+              </div>
+            </div>
+            <div className="product-list-list">
+              {currentProducts.map(product => (
+                <Product
+                  id={product.id}
+                  key={product.id}
+                  name={product.name}
+                  image={product.productImage}
+                  color={product.color}
+                  price={product.price}
+                  storage={product.storage}
+                  mainCamera={product.mainCamera}
+                  cpu={product.cpu}
+                />
+              ))}
+            </div>
+            <div className="pagination" style={{ gap: "8px", display: "flex" }}>
+              {Array.from({ length: totalPages }, (_, i) => (
+                <button
+                  style={{
+                    outline: "none", border: "0", cursor: "pointer", color:currentPage === i + 1 ? "white" : "black",
+                    fontSize: "16px", lineHeight: "24px", letterSpacing: "3%", fontWeight: "500",
+                    display: "flex", alignItems: "center", justifyContent: "center",
+                    background: currentPage === i + 1 ? "black" : "#F6F6F6", borderRadius: "5px",
+                    width: "32px", height: "32px"
+                  }}
+                  key={i}
+                  onClick={() => handlePageChange(i + 1)}
+                >
+                  {i + 1}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default ProductList;
